7399d6825c6e883fe938787b0a241ded41f31dd7
Juan García <43949658+garcia-s@users.noreply.github.com>
2024-12-05 14:37:43 -0500

    Error handling functions created

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+
+
+/**
+* Helper function for async error handling
+*
+* Returns an [Array] with two values [true] and [T] (The result of the callback promise)
+* Or [false] and an instance of the [Error] thrown by the function
+*
+* @remarks
+* For non async functions please use [pcallSync]
+*
+**/
+export async function pcall<T>(func: () => Promise<T>): Promise<[true, T] | [false, any]> {
+    try {
+        const result = await func();
+        return [true, result];
+    } catch (error) {
+        return [false, error];
+    }
+}
+
+
+/**
+* Helper function for error handling
+*
+* Returns an [Array] with two values [true] and [T] (The result of callback function)
+* Or [false] and an instance of the [Error] thrown by the
+*
+* @remarks
+* For  async functions please use [pcall]
+*
+**/
+export function pcallSync<T>(func: () => T): [true, T] | [false, any] {
+    try {
+        const result = func();
+        return [true, result]; // Successful execution
+    } catch (error) {
+        return [false, error]; // Catch any error
+    }
+}
+
